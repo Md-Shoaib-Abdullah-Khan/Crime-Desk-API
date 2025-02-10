22924be2ae8abc3df3985d2dfd002d9c00340298
Shoaib Khan <mdshoaib.a.khan@gmail.com>
2025-02-10 12:44:28 +0600

    commited

---
+++ Caption_Generator_API.py
@@ -0,0 +1,49 @@
+import requests
+from langchain_groq import ChatGroq
+from langchain.schema import SystemMessage, HumanMessage
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.environ['GROQ_API_KEY']
+
+app = Flask(__name__)
+
+def generate_caption(image_url):
+    chat = ChatGroq(api_key=API_KEY, model_name="llama-3.2-11b-vision-preview")
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Describe what crime is occuring in this image?"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": image_url
+                    }
+                }
+            ]
+        }
+    ]
+    
+    response = chat.invoke(messages)
+    return response.content
+
+@app.route('/generate_caption', methods=['POST'])
+def generate_caption_api():
+    data = request.json
+    image_url = data.get("image_url")
+    
+    if not image_url:
+        return jsonify({"error": "No image URL provided"}), 400
+    
+    caption = generate_caption(image_url)
+    return jsonify({"caption": caption})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
